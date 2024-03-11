df9ea888e6dbb6c5651409495fd89dbc6ee7bc2c
Xiangyu Xu <xuxiangy@amazon.com>
2024-03-11 14:32:49 -0700

    added Yul contract and tests

--- test/v1/Base_TestV1.t.sol
+++ test/v1/Base_TestV1.t.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {IHorseStore} from "../../src/horseStoreV1/IHorseStore.sol";
 import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
 import {Test, console2} from "forge-std/Test.sol";
 
 abstract contract Base_TestV1 is Test {
-    HorseStore public horseStore;
+    IHorseStore public horseStore;
 
     function setUp() public virtual {
-        horseStore = new HorseStore();
+        horseStore = IHorseStore(address(new HorseStore()));
     }
 
     function testReadValue() public {
